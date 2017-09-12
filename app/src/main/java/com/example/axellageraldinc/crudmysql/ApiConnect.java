36956b767bbb42a0a6510554a2d49db77255adfe
Axellageraldinc Adryamarthanino <axella.gerald@gmail.com>
2017-09-12 10:30:59 +0700

    - sudah bisa create dari android

--- app/src/main/java/com/example/axellageraldinc/crudmysql/ApiConnect.java
+++ app/src/main/java/com/example/axellageraldinc/crudmysql/ApiConnect.java
@@ -5,11 +5,11 @@ package com.example.axellageraldinc.crudmysql;
  */
 
 public class ApiConnect {
-    private static final String ipConfig = "http://10.72.41.102";
-    private static final String ROOT_URL = ipConfig + "/PHP_MySQL/api.php?apicall=";
+    public static final String ipConfig = "http://10.72.4.248";
+    public static final String ROOT_URL = ipConfig + "/PHP_MySQL/api.php?apicall=";
 
-    private static final String ROOT_URL_CREATE = ROOT_URL + "create";
-    private static final String ROOT_URL_READ = ROOT_URL + "getAll";
-    private static final String ROOT_URL_UPDATE = ROOT_URL + "update";
-    private static final String ROOT_URL_DELETE = ROOT_URL + "delete";
+    public static final String ROOT_URL_CREATE = ROOT_URL + "create";
+    public static final String ROOT_URL_READ = ROOT_URL + "getAll";
+    public static final String ROOT_URL_UPDATE = ROOT_URL + "update";
+    public static final String ROOT_URL_DELETE = ROOT_URL + "delete";
 }
--- app/src/main/java/com/example/axellageraldinc/crudmysql/MainActivity.java
+++ app/src/main/java/com/example/axellageraldinc/crudmysql/MainActivity.java
@@ -1,13 +1,93 @@
 package com.example.axellageraldinc.crudmysql;
 
+import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.ButtonBarLayout;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.HashMap;
 
 public class MainActivity extends AppCompatActivity {
+
+    private static final int CODE_GET_REQUEST = 1024;
+    private static final int CODE_POST_REQUEST = 1025;
+
+    private EditText txtName, txtPassword;
+    private Button btnInsert;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        txtName = (EditText) findViewById(R.id.txtUsername);
+        txtPassword = (EditText) findViewById(R.id.txtPassword);
+        btnInsert = (Button) findViewById(R.id.btnInsert);
+
+        btnInsert.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                insertData(txtName.getText().toString(), txtPassword.getText().toString());
+            }
+        });
+    }
+
+    private void insertData(String username, String password){
+        HashMap<String, String> param = new HashMap<>();
+        param.put("username", username);
+        param.put("password", password);
+
+        PerformNetworkRequest pnr = new PerformNetworkRequest(ApiConnect.ROOT_URL_CREATE, param, CODE_POST_REQUEST);
+        pnr.execute();
+    }
+
+    private class PerformNetworkRequest extends AsyncTask<Void, Void, String>{
+
+        String url;
+        HashMap<String, String> param;
+        int requestCode;
+
+        PerformNetworkRequest(String url, HashMap<String, String> param, int requestCode){
+            this.url = url;
+            this.param = param;
+            this.requestCode = requestCode;
+        }
+
+        @Override
+        protected void onPreExecute(){
+            super.onPreExecute();
+            Toast.makeText(MainActivity.this, "PLEASE WAIT, IN PROGRESS", Toast.LENGTH_SHORT).show();
+        }
+
+        @Override
+        protected String doInBackground(Void... voids) {
+            RequestHandler rh = new RequestHandler();
+            if (requestCode == CODE_POST_REQUEST){
+                return rh.sendPostRequest(url, param);
+            } else if(requestCode == CODE_GET_REQUEST){
+                return rh.sendGetRequest(url);
+            }
+            return null;
+        }
+
+        @Override
+        protected void onPostExecute(String s){
+            super.onPostExecute(s);
+            try{
+                JSONObject jsonObject = new JSONObject(s);
+                //Kalau gak response error kosong (alias gak error)
+                if (!jsonObject.getBoolean("error")){
+                    Toast.makeText(MainActivity.this, jsonObject.getString("message"), Toast.LENGTH_SHORT).show();
+                }
+            } catch (JSONException ex){
+                System.out.println("Error postExecute : " + ex.toString());
+            }
+        }
     }
 }
